add5da86f697214920b95f24f95076bc48edd83a
deibytb <deiby@devacademy.la>
2017-03-25 13:30:26 -0500

    Add basic view for mailing

--- mailing-turismoi/app/controllers/users_controller.rb
+++ mailing-turismoi/app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        UserMailer.frequent_user(@user).deliver_now
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
--- mailing-turismoi/app/mailers/user_mailer.rb
+++ mailing-turismoi/app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  default from: 'deibytb@outlook.com'
+
+  def frequent_user(user)
+    mail(to: user.email, subject: 'Recomienda y gana con turismoi')
+  end
 end
