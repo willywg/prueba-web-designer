8f1de0ed48676eae847fdf32d94052725e9b17ef
deibytb <deiby@devacademy.la>
2017-03-25 13:20:05 -0500

    Generate basic mailer

--- mailing-turismoi/config/application.rb
+++ mailing-turismoi/config/application.rb
@@ -11,5 +11,18 @@ module MailingTurismoi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Email configuration
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      authentication: :plain,
+      user_name: "cmactestica@gmail.com",
+      password: "abcDEF123*",
+      enable_starttls_auto: true,
+    }
   end
 end
